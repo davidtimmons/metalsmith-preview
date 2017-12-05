73bcbda7fdcf7e8a085b489ec98a33374138049b
David Timmons <github@timmons.io>
2017-12-04 23:56:08 -0600

    Configure project.

---
+++ test/index.spec.js
@@ -0,0 +1,15 @@
+/**
+ * Test the metalsmith-preview plugin.
+ */
+
+const { expect } = require('chai');
+const Preview = require('..');
+
+
+///////////
+// TESTS //
+///////////
+
+describe('metalsmith-preview', () => {
+    it();
+});
